463195d5597abfbed7262d1f3836fad8cd1d0a91
Jens Hviid <jens@hviid.ch>
2017-06-15 21:36:45 +0200

    Add links to have first version

--- src/App.js
+++ src/App.js
@@ -2,18 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class AppHeader extends Component{
+  render() {
+    return (
+        <div className="AppHeader">
+          <img src={logo} className="AppLogo" alt="logo" />
+        </div>
+    );
+  }
+}
+
+class AppIntro extends Component{
+  render() {
+    return (
+        <div className="AppIntro">
+          <p>
+            Welcome to arkcoin.net
+          </p>
+        </div>
+    );
+  }
+}
+
+class AppLinks extends Component{
+  render() {
+    return (
+        <div className="AppBody">
+          <ul>
+            <a href="https://ark.io/">ark.io</a>
+          </ul>
+          <ul>
+            <a href="https://www.reddit.com/r/ArkEcosystem/">reddit</a>
+          </ul>
+          <ul>
+            <a href="https://explorer.arkcoin.net/">explorer</a>
+          </ul>
+          <ul>
+            <a href="https://dexplorer.arkcoin.net/">devnet explorer</a>
+          </ul>          
+          <ul>
+            <a href="https://www.reddit.com/r/ArkEcosystem/wiki/jarunik">jarunik</a>
+          </ul>
+        </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-        <div className="App-intro">
-          <p>
-            Upcoming Dashboard
-          </p>
-        </div>
+        <AppHeader/>
+        <AppIntro/>
+        <AppLinks/>
       </div>
     );
   }
